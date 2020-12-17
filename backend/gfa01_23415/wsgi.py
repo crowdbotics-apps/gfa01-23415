b5e40feb56673c354d31560e7060dc257afb3351
Crowdbotics <team@crowdbotics.com>
2020-12-17 12:43:33 +0000

    Update models

--- backend/gfa01_23415/wsgi.py
+++ backend/gfa01_23415/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gfa01_23415.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gfa01_23415.settings")
 
 application = get_wsgi_application()
